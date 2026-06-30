c82ffcfb0e91bf6a4a86e8eb461b223dcaf6db7c
Ivan Karpoukhin <piromanster@gmail.com>
2026-06-30 17:17:22 +0300

    Update hb_heartbeat.cmd

--- hb_heartbeat.cmd
+++ hb_heartbeat.cmd
@@ -1,10 +1,6 @@
 @echo off
-setlocal
 set XML=C:\ProgramData\Novosoft\Handy Backup 8\Users\@WIN-D~1\Logs\reporting.xml
 
-set LATEST=1970-01-01 00:00:00
-for /f "tokens=2 delims=<>" %%a in ('findstr /c:"EndTime" "%XML%" 2^>nul') do (
-    set LATEST=%%a
-)
+if not exist "%XML%" exit /b
 
-if not "%LATEST%"=="1970-01-01 00:00:00" echo %LATEST%
+for /f %%a in ('powershell -Command "[math]::Round(((Get-Date) - (Get-Item '%XML%').LastWriteTime).TotalHours,1)"') do echo %%a
