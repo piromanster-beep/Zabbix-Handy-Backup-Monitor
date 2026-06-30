b72e1f63bc3f13221995052ab7be359f127373c8
Ivan Karpoukhin <piromanster@gmail.com>
2026-06-30 17:39:51 +0300

    Update hb_heartbeat.cmd

--- hb_heartbeat.cmd
+++ hb_heartbeat.cmd
@@ -3,4 +3,4 @@ set XML=C:\ProgramData\Novosoft\Handy Backup 8\Users\@WIN-D~1\Logs\reporting.xml
 
 if not exist "%XML%" exit /b
 
-for /f %%a in ('powershell -Command "[math]::Round(((Get-Date) - (Get-Item '%XML%').LastWriteTime).TotalHours,1)"') do echo %%a
+for /f %%a in ('powershell -Command "[math]::Floor(((Get-Date) - (Get-Item '%XML%').LastWriteTime).TotalHours)"') do echo %%a
